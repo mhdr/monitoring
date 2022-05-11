700b04256c3d801acd940d173693f0135d0f7ca9
Mahmood Ramzani <mahmoodramzani@gmail.com>
2022-05-12 00:16:34 +0430

    fix a bug in interface manager the prevents fetching required data from core because of core not running

--- interface_manager/src/main/java/net/pupli/interface_manager/services/CoreData.java
+++ interface_manager/src/main/java/net/pupli/interface_manager/services/CoreData.java
@@ -11,45 +11,70 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @Component
 public class CoreData implements CommandLineRunner {
 
     Logger logger = LoggerFactory.getLogger(CoreData.class);
 
+    private final ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+    boolean isSuccessful = false;
+
     @Override
     public void run(String... args) throws Exception {
-        try {
-            ConfigFile configFile = new ConfigFile();
-            String url = configFile.getCoreWebAddress() + "/api/interface/items";
 
-            RequestItemsDto requestDto = new RequestItemsDto();
-            Gson gson = new Gson();
-            String json = gson.toJson(requestDto);
+        // why we need rescheduling?
+        // because this app should run after core app, because we need some data from it
+        // we could simply introduce core app as interface manager dependency in systemd service file,
+        // but we can't do that because it's possible that core and interface manager are not running on the same server,
+        // so it's required because of scalability and distributed nature of the app
 
-            MediaType mediaType = MediaType.get("application/json;");
-            OkHttpClient client = new OkHttpClient();
-            RequestBody body = RequestBody.create(json, mediaType);
+        executorService.execute(() -> {
+            try {
+                while (!isSuccessful) {
+                    try {
+                        ConfigFile configFile = new ConfigFile();
+                        String url = configFile.getCoreWebAddress() + "/api/interface/items";
 
-            Request request = new Request.Builder()
-                    .url(url)
-                    .post(body)
-                    .build();
+                        RequestItemsDto requestDto = new RequestItemsDto();
+                        Gson gson = new Gson();
+                        String json = gson.toJson(requestDto);
 
-            Response response = client.newCall(request).execute();
-            String responseStr = response.body().string();
-            ResponseItemsDto responseDto = gson.fromJson(responseStr, ResponseItemsDto.class);
-            for (ResponseItemsDto.Item item : responseDto.getItems()) {
-                MyContext.myCache.getItems().put(item.getItemId(), item);
+                        MediaType mediaType = MediaType.get("application/json;");
+                        OkHttpClient client = new OkHttpClient();
+                        RequestBody body = RequestBody.create(json, mediaType);
+
+                        Request request = new Request.Builder()
+                                .url(url)
+                                .post(body)
+                                .build();
+
+                        Response response = client.newCall(request).execute();
+                        String responseStr = response.body().string();
+                        ResponseItemsDto responseDto = gson.fromJson(responseStr, ResponseItemsDto.class);
+                        for (ResponseItemsDto.Item item : responseDto.getItems()) {
+                            MyContext.myCache.getItems().put(item.getItemId(), item);
+                        }
+
+                        for (String credential : responseDto.getCredentials()) {
+                            MyContext.myCache.getCredentials().add(credential);
+                        }
+
+                        // make this variable true to exit the while loop
+                        isSuccessful = true;
+                    } catch (Exception ex) {
+                        logger.error(ex.getMessage(), ex);
+                    } finally {
+                        // reschedule every 1 second
+                        Thread.sleep(1000);
+                    }
+                }
+            } catch (Exception ex) {
+                logger.error(ex.getMessage(), ex);
             }
-
-            for (String credential : responseDto.getCredentials()) {
-                MyContext.myCache.getCredentials().add(credential);
-            }
-
-            logger.info(responseStr);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-
+        });
     }
 }
