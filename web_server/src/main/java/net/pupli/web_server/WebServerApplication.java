30aab399501ad333ff9169d7f8b22ef4e14e3da4
Mahmood Ramzani <mahmoodramzani@gmail.com>
2022-05-20 13:04:38 +0430

    setting up web server project

--- web_server/src/main/java/net/pupli/web_server/WebServerApplication.java
+++ web_server/src/main/java/net/pupli/web_server/WebServerApplication.java
@@ -2,8 +2,10 @@ package net.pupli.web_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "net.pupli.web_server.repositories")
 public class WebServerApplication {
 
     public static void main(String[] args) {
--- web_server/src/test/java/net/pupli/web_server/WebServerApplicationTests.java
+++ web_server/src/test/java/net/pupli/web_server/WebServerApplicationTests.java
@@ -3,11 +3,11 @@ package net.pupli.web_server;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+/*@SpringBootTest
 class WebServerApplicationTests {
 
     @Test
     void contextLoads() {
     }
 
-}
+}*/
