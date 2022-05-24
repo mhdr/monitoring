8719ab35be1401b34ea358eddc81bdf59adefaba
Mahmood Ramzani <mahmoodramzani@gmail.com>
2022-05-25 01:16:14 +0430

    deploy config file

--- configs/deploy.sh
+++ configs/deploy.sh
@@ -3,4 +3,5 @@
 mkdir -p ~/.monitoringv5
 cp core.config ~/.monitoringv5/core.config
 cp interface_manager.config ~/.monitoringv5/interface_manager.config
+cp web_server.config ~/.monitoringv5/web_server.config
 ls ~/.monitoringv5
