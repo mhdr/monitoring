f6c4d6942d7b3208a1304300d97f6cdfc07b2847
Mahmood Ramzani <mahmoodramzani@gmail.com>
2022-06-15 01:06:07 +0430

    remove junks and projects which are no longer needed

--- web_server/src/main/java/net/pupli/web_server/repositories/AlarmHistoryRealRepository.java
+++
@@ -1,6 +0,0 @@
-package net.pupli.web_server.repositories;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AlarmHistoryRealRepository extends MongoRepository<AlarmHistoryRealRepository, String> {
-}
